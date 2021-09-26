2ec76a24171f3e21a33d2c4fadd123a4d2bbea4d
ipe <izaki.yb1998@gmail.com>
2021-09-27 08:20:00 +0900

    top追加

---
+++ src/views/Top.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div v-for="(memo, index) in memos" :key="index">
+            <router-link :to="{name:'memo',params:{id:memo.ID}}">{{memo.Title}}</router-link>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'Top',
+    
+    data() {
+        return {
+            colors: ["red","blue"],
+            memos: []
+        }
+    },
+    mounted() {
+        this.$store.dispatch(
+            'http/request',
+            {
+                url:'/api/memo/get/list',
+                data:''
+            }
+        ).then(function(res) {
+            res.forEach(element => {
+                // console.log(element)
+                this.memos.push(element)
+            });
+            console.log(this.memos)
+            
+            
+        }.bind(this))
+    },
+    
+}
+</script>
